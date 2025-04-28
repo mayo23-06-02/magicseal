ac7dfed54a07e52173cb98c99e43e2f3f515f109
Mayo <makabmahlalela@gmail.com>
2025-04-28 08:59:13 +0200

    Update LandingSection01.js

--- components/LandingPage/LandingSection01.js
+++ components/LandingPage/LandingSection01.js
@@ -128,8 +128,8 @@ function LandingSection01() {
                 quality={90}
               />
               <div className="absolute inset-0 bg-black bg-opacity-30">
-                <div className="container mx-auto px-4 lg:px-8 h-full flex items-center">
-                  <div className="max-w-7xl mx-auto text-white">
+                <div className="container mx-auto px-4 lg:px-8 h-full flex items-center justify-center">
+                  <div className="max-w-7xl mx-auto flex flex-col items-center justify-center text-white">
                     {slide.productLogo && (
                       <Image
                         src={slide.productLogo}
